c305a0d908eea7eac19c82cfe5cd115f130a8122
markuz899 <markuz.89@hotmail.it>
2022-05-19 18:10:39 +0200

    add user-permissions

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,24 +1,3 @@
-const crypto = require("crypto");
-const _ = require("lodash");
-const emailRegExp =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const formatError = (error) => [
-  { messages: [{ id: error.id, message: error.message, field: error.field }] },
-];
+'use strict';
 
-module.exports = (plugin) => {
-  plugin.controllers.user.login = async (ctx) => {
-    console.log("login service");
-  };
-
-  plugin.routes["content-api"].routes.push({
-    method: "POST",
-    path: "/:store/auth/local",
-    handler: "user.login",
-    config: {
-      prefix: "",
-    },
-  });
-
-  return plugin;
-};
+module.exports = require('./server');
