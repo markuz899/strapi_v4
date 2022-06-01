e71936d6bd5d0a50fab214cf11a5da0dfa65d45b
markuz899 <markuz.89@hotmail.it>
2022-06-01 08:59:29 +0200

    add routes for leads refine

--- src/api/lead/controllers/lead.js
+++ src/api/lead/controllers/lead.js
@@ -1,4 +1,3 @@
-const { getService } = require("@strapi/plugin-users-permissions/server/utils");
 const entity = "api::lead.lead";
 
 module.exports = {
@@ -26,5 +25,40 @@ module.exports = {
     return {
       status: true,
     };
+  },
+
+  async findRefine(ctx) {
+    ctx.query = { ...ctx.query };
+
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  },
+
+  async findOneRefine(ctx) {
+    const { id } = ctx.params;
+    const { query } = ctx;
+
+    const lead = await strapi.service(entity).findOne(id, query);
+    const sanitizedEntity = await this.sanitizeOutput(lead, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+
+  async updateOneRefine(ctx) {
+    const { id } = ctx.params;
+    const { body } = ctx.request;
+
+    const result = await strapi.service(entity).update(id, body);
+
+    return result;
+  },
+
+  async deleteOneRefine(ctx) {
+    const { id } = ctx.params;
+    const lead = await strapi.service(entity).delete(id);
+
+    const sanitizedEntity = await this.sanitizeOutput(lead, ctx);
+    return this.transformResponse(sanitizedEntity);
   },
 };
--- src/api/lead/routes/lead.js
+++ src/api/lead/routes/lead.js
@@ -5,5 +5,25 @@ module.exports = {
       path: "/:store/leads",
       handler: "lead.create",
     },
+    {
+      method: "GET",
+      path: "/leads",
+      handler: "lead.findRefine",
+    },
+    {
+      method: "GET",
+      path: "/leads/:id",
+      handler: "lead.findOneRefine",
+    },
+    {
+      method: "PUT",
+      path: "/leads/:id",
+      handler: "lead.updateOneRefine",
+    },
+    {
+      method: "DELETE",
+      path: "/leads/:id",
+      handler: "lead.deleteOneRefine",
+    },
   ],
 };
