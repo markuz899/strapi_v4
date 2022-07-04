177ddd4fb3f47b49f92f8b79f5bbfea9d92dea26
markuz899 <markuz.89@hotmail.it>
2022-07-04 15:39:13 +0200

    use nodemailer to send email provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,31 @@
 module.exports = ({ env }) => ({
   email: {
+    // config: {
+    //   provider: "sendgrid",
+    //   providerOptions: {
+    //     apiKey: env("SEND_GRID_KEY"),
+    //   },
+    //   settings: {
+    //     defaultFrom: "markuz.89@hotmail.it",
+    //     defaultReplyTo: "markuz.89@hotmail.it",
+    //     testAddress: "markuz.89@hotmail.it",
+    //   },
+    // },
     config: {
-      provider: "sendgrid",
+      provider: "nodemailer",
       providerOptions: {
-        apiKey: env("SEND_GRID_KEY"),
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: env("EMAIL"),
+          clientId: env("CLIENT_ID"),
+          clientSecret: env("CLIENT_SECRET"),
+          refreshToken: env("REFRESH_TOKEN"),
+        },
       },
       settings: {
-        defaultFrom: "markuz.89@hotmail.it",
-        defaultReplyTo: "markuz.89@hotmail.it",
-        testAddress: "markuz.89@hotmail.it",
+        defaultFrom: `SGASGAS - INFO <${env("EMAIL")}>`,
+        defaultReplyTo: env("EMAIL"),
       },
     },
   },
