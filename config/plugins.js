93212b2abd952f4a69a1769e786c67e4b1562ba4
markuz899 <markuz.89@hotmail.it>
2022-07-04 20:21:25 +0200

    use sendgrid plugin email

--- config/plugins.js
+++ config/plugins.js
@@ -1,35 +1,35 @@
 module.exports = ({ env }) => ({
   email: {
-    // config: {
-    //   provider: "sendgrid",
-    //   providerOptions: {
-    //     apiKey: env("SEND_GRID_KEY"),
-    //   },
-    //   settings: {
-    //     defaultFrom: "markuz.89@hotmail.it",
-    //     defaultReplyTo: "markuz.89@hotmail.it",
-    //     testAddress: "markuz.89@hotmail.it",
-    //   },
-    // },
     config: {
-      provider: "nodemailer",
+      provider: "sendgrid",
       providerOptions: {
-        service: "gmail",
-        host: env("SMTP_HOST", "smtp.google.com"),
-        port: env("SMTP_PORT", 587),
-        secure: false,
-        auth: {
-          type: "OAuth2",
-          user: env("EMAIL"),
-          clientId: env("CLIENT_ID"),
-          clientSecret: env("CLIENT_SECRET"),
-          refreshToken: env("REFRESH_TOKEN"),
-        },
+        apiKey: env("SEND_GRID_KEY"),
       },
       settings: {
-        defaultFrom: `SGASGAS - INFO <${env("EMAIL")}>`,
-        defaultReplyTo: env("EMAIL"),
+        defaultFrom: `SGASGAS - INFO <${env("SEND_GRID_DEFAULT_FROM")}>`,
+        defaultReplyTo: env("SEND_GRID_DEFAULT_FROM"),
+        testAddress: env("SEND_GRID_DEFAULT_FROM"),
       },
     },
+    // config: {
+    //   provider: "nodemailer",
+    //   providerOptions: {
+    //     service: "gmail",
+    //     host: env("SMTP_HOST", "smtp.google.com"),
+    //     port: env("SMTP_PORT", 587),
+    //     secure: false,
+    //     auth: {
+    //       type: "OAuth2",
+    //       user: env("EMAIL"),
+    //       clientId: env("CLIENT_ID"),
+    //       clientSecret: env("CLIENT_SECRET"),
+    //       refreshToken: env("REFRESH_TOKEN"),
+    //     },
+    //   },
+    //   settings: {
+    //     defaultFrom: `SGASGAS - INFO <${env("EMAIL")}>`,
+    //     defaultReplyTo: env("EMAIL"),
+    //   },
+    // },
   },
 });
